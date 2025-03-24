a483364df938259c85276d30ddbd21a632a7bccf
poneding <poneding@gmail.com>
2025-03-24 22:30:16 +0800

    implement enum for HTTP 4xx Client Errors in q04_01.rs

--- src/bin/q04_01.rs
+++ src/bin/q04_01.rs
@@ -3,4 +3,38 @@
 /// Create an enum that contains HTTP 4xx Client Errors.
 /// `i.e.` `Not Found` Should have value `404` associated with it.
 fn main() {
+    let not_found = HttpStatusCode::NotFound;
+    assert_eq!(not_found as i32, 404);
+}
+
+#[allow(dead_code)]
+enum HttpStatusCode {
+    NotFound = 404,
+    BadRequest = 400,
+    Unauthorized = 401,
+    PaymentRequired = 402,
+    Forbidden = 403,
+    MethodNotAllowed = 405,
+    NotAcceptable = 406,
+    ProxyAuthenticationRequired = 407,
+    RequestTimeout = 408,
+    Conflict = 409,
+    Gone = 410,
+    LengthRequired = 411,
+    PreconditionFailed = 412,
+    PayloadTooLarge = 413,
+    URITooLong = 414,
+    UnsupportedMediaType = 415,
+    RangeNotSatisfiable = 416,
+    ExpectationFailed = 417,
+    ImATeapot = 418,
+    MisdirectedRequest = 421,
+    UnprocessableEntity = 422,
+    Locked = 423,
+    FailedDependency = 424,
+    UpgradeRequired = 426,
+    PreconditionRequired = 428,
+    TooManyRequests = 429,
+    RequestHeaderFieldsTooLarge = 431,
+    UnavailableForLegalReasons = 451,
 }
