9a0bf4822efc14f726efc48172fce562807eb336
poneding <poneding@gmail.com>
2025-03-24 22:30:28 +0800

    implement enum to represent Child and Adult based on age in q04_02.rs

--- src/bin/q04_02.rs
+++ src/bin/q04_02.rs
@@ -2,4 +2,15 @@
 ///
 /// Write an enum to store information of whether a person is a child or adult based on his/her age.
 fn main() {
+    let _child = Person::Child;
+
+    let adult = Person::Adult(25);
+    if let Person::Adult(age) = adult {
+        println!("Adult with age: {}", age);
+    }
+}
+
+enum Person {
+    Child,
+    Adult(u8), // Adult with age
 }
